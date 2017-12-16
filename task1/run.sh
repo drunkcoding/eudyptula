c5212ff2f5b2649bc694aadb97e1eba8d9ea0f5b
Leyang(Nick) Xue <xue.thinkingreed@gmail.com>
2017-12-16 19:16:12 +0800

    final

--- task1/run.sh
+++ task1/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sudo dmesg -c
 make
 sudo insmod `ls | grep -E .+\.ko`
-dmesg | grep ve482
 read -s -n1 -p "Press any key to continue ... "
 sudo rmmod -f `ls | grep -E .+\.ko`
-echo "Module removed"
-dmesg | grep ve482
 make clean
--- task5/run.sh
+++ task5/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make
-echo 'ACTION=="add", SUBSYSTEM=="usb", RUN+="/sbin/insmod '`pwd`'/helloworld.ko"\n' > hobby-usb.rules
-echo 'ACTION=="remove", SUBSYSTEM=="usb", RUN+="/sbin/rmmod '`pwd`'/helloworld.ko"\n' >> hobby-usb.rules
+echo 'ACTION=="add", SUBSYSTEM=="usb", RUN+="/sbin/insmod '`pwd`'/helloworld.ko"'\n > hobby-usb.rules
+echo 'ACTION=="remove", SUBSYSTEM=="usb", RUN+="/sbin/rmmod '`pwd`'/helloworld.ko"' >> hobby-usb.rules
 sudo cp hobby-usb.rules /etc/udev/rules.d/
 read -s -n1 -p "Press any key to continue ... "
 sudo rm -f /etc/udev/rules.d/hobby-usb.rules
